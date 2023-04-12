5f877ab81c91e6c5be2c3d5e688479b955fef8e6
simon <simon.schoeggler@hotmail.com>
2023-04-12 08:50:34 +0200

    Initial commit

---
+++ src/main/java/at/kaindorf/data/Tester.java
@@ -0,0 +1,11 @@
+package at.kaindorf.data;
+/**
+* <h3>Created by IntelliJ IDEA.</h3><br>
+* <b>Project:</b> Streams<br>
+* <b>User:</b> Simon Schoeggler<br>
+* <b>Date:</b> 12. April 2023<br>
+* <b>Time:</b> 08:15<br>
+*/
+
+public class Tester {
+}
